9722a61afb6a033e248d50b241958f96f8ee4e21
mahshid <bodaqi.zahra@gmail.com>
2026-08-29 10:46:06 +0330

    chore: save local nx setup changes before merge

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Test } from './test/test';
-import { Product } from './test/product/product';
+import { Product } from '@onlineShop/models';
+
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,12 @@ import { Product } from './test/product/product';
 })
 export class App {
   protected readonly title = signal('my-first-app');
+  sampleProduct: Product = {
+    id: 1,
+    title: 'تست محصول Nx Monorepo',
+    price: 150000,
+    description: 'تست لایبرری مشترک',
+    category: 'آزمایشی',
+    image: 'test.png',
+  };
 }
